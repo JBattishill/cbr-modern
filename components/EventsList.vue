728b0df0c95a22e4727757ed15ccff976c4cfe65
JBattishill <jbattishill91@gmail.com>
2022-08-31 16:17:29 +1000

    Style css

--- components/EventsList.vue
+++ components/EventsList.vue
@@ -1,5 +1,6 @@
 <!-- this component returns a list of all the buildings -->
 <template>
+    <div class="container.xxl events m-5">
 	<div class="">
 		<!-- these would show while the data loads or if an error -->
 		<p v-if="$fetchState.pending">Loading buildings...</p>
@@ -7,25 +8,29 @@
 
 		<ul v-else class="list-group">
 			<!-- this is a for loop, it just loops through the list of buildings returned from the API -->
-			<li v-for="events in events" :key="events.id" class="list-group-item">
+			<li v-for="events in events" :key="events.id" class="list-group-item blue">
 				<!-- now make a link for each item -->
-				<!-- <NuxtLink :to="building.slug"> -->
+				<!-- <NuxtLink :to="events.slug"> -->
 				<NuxtLink :to="'/events/' + events.slug">
 					<!-- return the rendered title -->
 					{{  events.title.rendered  }}
 				</NuxtLink>
 				<!-- now show me the building year -->
-				: {{ events.acf.year }}
+				: {{ events.date }}
 			</li>
 		</ul>
 		<h3>this list is stored in components/EventsList.vue</h3>
-
+    </div>
 	</div>
 </template>
 
+<style scoped>
+    @import url('~assets/css/style.css');
+</style>
 
 <script>
 export default {
+	
 	// layout: 'home',
 	async fetch() {
 		this.events = await fetch('http://cm.beneb.com/wp-json/wp/v2/events/?per_page=100').then((res) =>
