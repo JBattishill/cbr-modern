d94a11359a1af183f780a0f11936b75743914bb6
JBattishill <jbattishill91@gmail.com>
2022-09-14 21:46:40 +1000

    Testing hosting with Netlify

--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,12 +1,8 @@
-
 <template>
-	<div class="container">
-		<h2>{{ $route.params.slug }}</h2>
-		<h3>Filename: pages/_pages.vue</h3>
-
+	<div class="">
 		<SiteNavigation />
-
-		<div class="page">
+		<h2>{{ $route.params.slug }}</h2>
+		<div class="content videos">
 			<!-- the data returns an array with one item in it, so need to reference it below -->
 			<h2>{{  page[0].title.rendered  }}</h2>
 
@@ -14,7 +10,6 @@
 			<!-- if we don't use the v-html tag it will render it as a string -->
 			<div v-html="renderedContent"></div>
 
-			<!-- <pre>{{ $data }}</pre> -->
 		</div>
 	</div>
 </template>
@@ -26,7 +21,7 @@ export default {
 	async asyncData({ params }) {
 		//page returns the data so we can access it below
 		const page = await fetch(
-			`http://cm.beneb.com/wp-json/wp/v2/pages/?slug=about`
+			`http://cm.beneb.com/wp-json/wp/v2/pages/?slug=videos`
 		).then((res) => {
 			if (res.ok) {
 				return res.json()
